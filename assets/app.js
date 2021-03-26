aef689627cd4419aa042c04dae0be1ca246071b1
Jacky Tong <tongjac@yahoo.com>
2021-03-26 16:31:55 -0700

    Added click event in script and formatted front end

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,24 @@
 let weatherArray = [];
+
+let weatherCall =
+  "api.openweathermap.org/data/2.5/weather?q=London&appid=" + "apiKey";
+
+function trigger() {
+  fetch(weatherUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((requestData) => {
+      if (cityInputVal !== requestData.name) {
+        alert("It's not a city. Please enter the name of the city."); // alerts the user they entered an unknown city
+        return;
+      }
+    });
+}
+
+document
+  .getElementById("submit-button")
+  .addEventListener("click", function (e) {
+    e.preventDefault;
+    console.log("clicked");
+  });
