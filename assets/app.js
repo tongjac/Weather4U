1127e3b19fb74d3b05a69aae652d55b48734eff6
Jacky Tong <tongjac@yahoo.com>
2021-03-26 17:24:37 -0700

    Fixing and processing response

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,32 @@
 let weatherArray = [];
-
-// let cityInput = document.getElementById("search-form").value;
-
-let weatherCall =
-  "api.openweathermap.org/data/2.5/weather?q=London&appid=" + "apiKey";
-
-function trigger() {
-  fetch(weatherUrl)
-    .then((response) => {
-      return response.json();
-    })
-    .then((requestData) => {
-      if (cityInputVal !== requestData.name) {
-        alert("It's not a city. Please enter the name of the city."); // alerts the user they entered an unknown city
-        return;
-      }
-    });
-}
+let cityInput = "";
+let apiKey = "c1652b344dd0c23882d04fcb84c0b87a";
+let array = [];
 
 document
   .getElementById("submit-button")
   .addEventListener("click", function (e) {
     e.preventDefault;
-    console.log(document.getElementById("search-form").value);
+    cityInput = document.getElementById("search-form").value;
+    trigger(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        cityInput +
+        "&appid=" +
+        apiKey
+    );
   });
+
+function trigger(url) {
+  fetch(url)
+    .then((response) => {
+      array = response.json();
+      console.log(array);
+      console.log(this.main.feels_like);
+      console.log(this.clouds);
+      console.log(this.timezone);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
